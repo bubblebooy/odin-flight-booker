53cc87f900c6b6c9a34394701d0fb8a74f0f699c
Max Garber <bubblebooy@gmail.com>
2019-03-03 23:34:47 -0600

    updating stuff to redeploy to heroku

--- db/migrate/20180914141018_create_flights.rb
+++ db/migrate/20180914141018_create_flights.rb
@@ -1,8 +1,8 @@
 class CreateFlights < ActiveRecord::Migration[5.2]
   def change
     create_table :flights do |t|
-      t.references :departure_airport_id, foreign_key: true
-      t.references :arrival_airport_id, foreign_key: true
+      t.references :departure_airport, foreign_key: true
+      t.references :arrival_airport, foreign_key: true
       t.datetime :departure_time
       t.timestamps
     end
