5312425a28e7fe10a0f34b7d611fefb16930d4b7
Max Garber <bubblebooy@gmail.com>
2019-03-03 21:44:04 -0600

    updating stuff to redeploy to heroku

--- db/migrate/20180914141018_create_flights.rb
+++ db/migrate/20180914141018_create_flights.rb
@@ -1,11 +1,20 @@
 class CreateFlights < ActiveRecord::Migration[5.2]
   def change
     create_table :flights do |t|
-      t.references :departure_airport, foreign_key: true
-      t.references :arrival_airport, foreign_key: true
+      t.integer :departure_airport_id, foreign_key: true
+      t.integer :arrival_airport_id, foreign_key: true
       t.datetime :departure_time
-
       t.timestamps
     end
+
+    # add_foreign_key :flights,
+    #                 :airports,
+    #                 column: :departure_airport_id
+    # add_index :flights, :departure_airport_id
+    # add_foreign_key :flights,
+    #                 :airports,
+    #                 column: :arrival_airport_id
+    # add_index :flights, :arrival_airport_id
+
   end
 end
--- db/migrate/20180916212655_create_bookings.rb
+++ db/migrate/20180916212655_create_bookings.rb
@@ -1,10 +1,20 @@
 class CreateBookings < ActiveRecord::Migration[5.2]
   def change
     create_table :bookings do |t|
-      t.references :flight, foreign_key: true
-      t.references :passenger, foreign_key: true
+      t.integer :flight, foreign_key: true
+      t.integer :passenger, foreign_key: true
 
       t.timestamps
     end
+
+    # add_foreign_key :bookings, 
+    #             :airports,
+    #             column: :departure_airport_id
+    # add_index :flights, :departure_airport_id
+    # add_foreign_key :bookings,
+    #                 :airports,
+    #                 column: :arrival_airport_id
+    # add_index :flights, :arrival_airport_id
+
   end
 end
